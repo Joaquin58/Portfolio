8e5acc03cb3eb64367fd3e4d14ff1e1d5d7378c2
Joaquin58 <91435597+Joaquin58@users.noreply.github.com>
2022-06-30 22:57:06 -0500

    feat: configuración de las rutas para la creacion de usuarios y login del usuario

--- api/src/controllers/admin.ts
+++ api/src/controllers/admin.ts
@@ -3,25 +3,17 @@ import User from "../models/user"
 import { auth, createuser } from "./types/auth"
 import jwt from "jsonwebtoken"
 import config from "../lib/config"
-import { compare, hash, genSalt } from "bcrypt";
+
 
 const route = Router()
+
+//todo: crea el login pero el con la comparacion dentro del modelo y retorna un metodo nuevo de la instancia
 
 route.post("/login", async (req: Request, res: Response) => {
     // const { username, password }: { username: string, password: string } = req.body
     const { username, password }: auth = req.body
-    //!cambiar la busqueda de usuario por la password
     const user = await User.findOne({ username })
-    console.log(user)
-// ! cehckt the error in compare
-    let match = true
-    !(user === null) && (match = await compare(password, user.password))
-    console.log(match)
-
-    const passwordcorrect = user === null
-        ? false
-        : await compare(password, user.password)
-
+    const passwordcorrect = await user?.login({ password })
     if (!(user && passwordcorrect)) return res.status(401).json({
         error: "invalid user or password"
     })
@@ -43,14 +35,53 @@ route.post("/login", async (req: Request, res: Response) => {
     })
 })
 
+//todo: crea el login pero el con la comparacion dentro de la ruta
+// route.post("/login", async (req: Request, res: Response) => {
+//     // const { username, password }: { username: string, password: string } = req.body
+//     const { username, password }: auth = req.body
+//     const user = await User.findOne({ username })
+//     const passwordcorrect = user === null
+//         ? false
+//         : await compare(password, user.password)
+
+//     if (!(user && passwordcorrect)) return res.status(401).json({
+//         error: "invalid user or password"
+//     })
+
+//     const userForToken: { id: string, username: string } = {
+//         id: user._id,
+//         username: user.username
+//     }
+//     if (config.PrivetKey1.length === 0) return res.status(404).json({ msg: "privetkey no porporcionada" })
+//     const token = jwt.sign(userForToken, config.PrivetKey1, {
+//         expiresIn: 60 * 60 * 9
+//     })      //!palabra secreta en variables de entorno
+
+
+//     res.status(200).send({
+//         name: user?.firstname,
+//         username: user?.username,
+//         token
+//     })
+// })
+
+//todo: primero hashea y despues crea el modelo
+// route.post("/create", (req: Request, res: Response) => {
+//     const body: createuser = req.body
+//     genSalt()
+//         .then(response => hash(body.password, response))
+//         .then(response => User.create({ ...body, password: response }))
+//         .then(() => res.status(200).json({
+//             msg: "usuario creado"
+//         }))
+//         .catch(err => res.status(500).json(err))
+// })
+
+//todo: creando y hash antes del modelo
 route.post("/create", (req: Request, res: Response) => {
     const body: createuser = req.body
-    User.find(body).then((response) => {
-        console.log(response)
-    })
     User.create(body)
-        .then((response) => response.save())
-        .then((response) => res.status(200).json({
+        .then(() => res.status(200).json({
             msg: "usuario creado"
         }))
         .catch(err => res.status(500).json(err))
